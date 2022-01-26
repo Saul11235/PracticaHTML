5629859203f885c1e7b1f56612ec3b94df6e4d2f
Edwin Saul <edwinsaulpm@gmail.com>
2022-01-25 22:24:43 -0500

    editando archivos js

---
+++ ejemplosJS/001_holamundoJS/HolaMundo.js
@@ -0,0 +1,2 @@
+//*ejemplo de hola mundo desde javascript*/
+console.log("Hola Mundo desde javascript")
---
+++ ejemplosJS/002_tiposDatosJS/TiposDeDatos.js
@@ -0,0 +1,6 @@
+//El primer tipo de dato analizado que veremos sera el strin
+// se separa entre comillas simples o dobles
+var texto1="Hola esto es una cadena de texto"
+var texto2='Hola este es otro texto'
+console.log(texto1)
+console.log(texto2)
