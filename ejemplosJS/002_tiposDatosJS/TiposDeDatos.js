432e56508b37c88b1594ea6010b84902244aaf02
Edwin Saul <edwinsaulpm@gmail.com>
2022-01-26 00:22:40 -0500

    edit tipos datos js

--- ejemplosJS/002_tiposDatosJS/TiposDeDatos.js
+++ ejemplosJS/002_tiposDatosJS/TiposDeDatos.js
@@ -1,6 +1,20 @@
 //El primer tipo de dato analizado que veremos sera el strin
+
 // se separa entre comillas simples o dobles
 var texto1="Hola esto es una cadena de texto"
 var texto2='Hola este es otro texto'
 console.log(texto1)
 console.log(texto2)
+
+// valores booleanos
+var verdadero=true
+var falso=false
+
+//variable nulo
+var nulo=null
+
+//variable no definida
+var nodefinida=undefined
+
+
+
