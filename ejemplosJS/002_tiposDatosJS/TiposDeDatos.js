58cfb720d9ad4e05bbffcafafd2ca935add8871b
Edwin Saul <edwinsaulpm@gmail.com>
2022-01-27 23:47:17 -0500

    ejemplos basicos javascript

--- ejemplosJS/002_tiposDatosJS/TiposDeDatos.js
+++ ejemplosJS/002_tiposDatosJS/TiposDeDatos.js
@@ -6,20 +6,53 @@ var texto2='Hola este es otro texto'
 console.log(texto1)
 console.log(texto2)
 
+
 // valores booleanos
 var verdadero=true
 var falso=false
+console.log(verdadero,falso)
+
 
 //variable nulo o vacio
 var nulo=null
+console.log(nulo)
+
 
 //variable no definida
+//NOTA. en caso no se indique el tipo tambien aplica
 var nodefinida=undefined
+console.log(nodefinida)
+
 
 //variable tipo numerica
 var numero=Number(1)
+var otronum=21
+var unNumMas=333.33
+console.log(numero,otronum,unNumMas)
+//en este lenguaje los enteros y floats son tratados como un mismo tipo
+//de objeto :/ 
+
 
 //variable tipo object
 // son estructuras mas complejas para definir agrupan a lso tipos anteriores
+var miPrimerObjeto={}/*obj vacio*/
+var miSegundoObjeto={
+  //asignando valores al objeto
+  unNumero:11,
+  unSting:"Hola soy un string",
+  soyUnaCond:true,
+}
+console.log(miPrimerObjeto,miSegundoObjeto)
+//podemos acceder a las propiedades del objeto
+console.log(miSegundoObjeto.unNumero)
 
 
+//variale tipo array
+//listas estructuras paara enumerar objetos
+var arrayVacio=[]
+var arrayConContenido=[2,3,"hola"]
+console.log(arrayVacio,arrayConContenido)
+//puede tener dentro otros arrays objetos etc
+//se pueden ahcer acctiones como agregar
+arrayConContenido.push("nuevo")
+console.log(arrayConContenido)
---
+++ ejemplosJS/003_trabajando_con_variables/variables.js
@@ -0,0 +1,16 @@
+//para definir una variable hay dos maneras de hacerlo
+//
+//    var --> VARIABLE es una variable que puede cambiar de valor de alcance local
+//
+//    let --> VARIABLE que solo sera validaa en el bloque donde se declara
+//
+//    cons --> CONSTANTE no va a cambiar su valor en el futurok
+//
+
+//declarando variable
+let miVariable="Hola como estas :) "
+console.log(miVariable)
+
+//cambiando la variable  -- mutabilidad
+miVariable="cambiaste la receta :O"
+console.log(miVariable)
