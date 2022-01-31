b0f2f02f14843cb85003c49faa28d531e0a90c6b
Edwin Saul <edwinsaulpm@gmail.com>
2022-01-31 17:48:08 -0500

    operadores js

---
+++ ejemplosJS/004_operadoresJS/operadoresJS.js
@@ -0,0 +1,38 @@
+//en esta practica veremos los operadores basicos en JS
+
+//operador SUMA
+var suma=1+2
+console.log("Suma ",suma)
+
+//operador RESTA
+var resta=100-1
+console.log("Resta ",resta)
+
+//operador MULTIPLICACION
+var multiplicacion=11*11
+console.log("Multiplicacion ",multiplicacion)
+
+//operador MODULO equivalente al residuo
+var modulo=99%10
+console.log("Modulo",modulo)
+
+//vaciariones ++ -- 
+console.log("----------------------------------")
+var numero=1
+console.log("Numero inicial:",numero)
+numero++                                 //aumenta +1
+console.log("Numero++:",numero)
+numero--                                 //disminuye -1
+console.log("Numero--:",numero)
+numero += 100                            //aumenta +100
+console.log("Numero+100:",numero)
+numero -=50                              //disminuye -50
+console.log("Numero-50:",numero)
+numero /=2                               //divide/2
+console.log("numero/2:",numero)
+numero *= 3                              //multiplica*3 
+
+
+
+
+
