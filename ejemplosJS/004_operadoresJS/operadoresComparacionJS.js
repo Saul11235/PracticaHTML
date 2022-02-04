b07f7c4568ce2e871328e1c8f158aec73a7b2bf8
Edwin Saul <edwinsaulpm@gmail.com>
2022-02-03 23:45:44 -0500

    operadoresComparacionJS

---
+++ ejemplosJS/004_operadoresJS/operadoresComparacionJS.js
@@ -0,0 +1,12 @@
+//operadores de comparacion en js
+//analizando el uso de comparadores estrictos
+//
+//
+var comparacion1 = 2 ==="2"
+console.log("comparacion estricta 2==='2'",comparacion1)
+
+var comparacion2 = 2=="2"
+console.log("comparacion permisiva 2=='2'",comparacion2)
+
+
+
