21e1e9f31a8beb8b6101064b1c5b4fa04a2aa261
Edwin Saul <edwinsaulpm@gmail.com>
2022-02-07 19:57:58 -0500

    flujo logico  arreglos

--- ejemplosJS/005_flujoLogicoJS/Manejando_ArreglosJS.js
+++ ejemplosJS/005_flujoLogicoJS/Manejando_ArreglosJS.js
@@ -1,6 +1,22 @@
 //Manejando y llamando objetos dentro de arreglos
 
-
 let MiArreglo=[11,22,32,44.4,555,66,7,"8","9999",1010]
+//Averiguando el taman'o del arreglo
+let tamano=MiArreglo.length
 
-
+//---------------------------------------------------
+console.log("\nAnalizando un arreglo")
+console.log(MiArreglo)
+console.log("Numero de elementos:",tamano,"\n")
+//--------------------------------------------------
+for (let i = 0; i < tamano ; i++) {
+  console.log(" item nro:",i," = ",MiArreglo[i])
+}
+//--------------------------------------------------
+console.log("\nAumentando elemento 2200")
+MiArreglo.push(2200)  //<------PUSH
+console.log(MiArreglo)
+//---------------------------------------------------
+console.log("\nQuitando Ultimo elemento")
+MiArreglo.pop()      //<-------POP
+console.log(MiArreglo)
---
+++ ejemplosJS/006_funcionesJS/1_funciones_basicasJS.js
@@ -0,0 +1,12 @@
+//Funciones en javascript
+//
+function saludar() {
+  console.log("\n SALUDO :) \n")
+}
+//vamos a llamar tres veces el saludo
+console.log(">Llamando tres veces el saludo")
+
+saludar()
+saludar()
+saludar()
+
