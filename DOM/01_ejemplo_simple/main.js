aaf45f345578f4b17bbdb7cd8a844426bc0a7e36
Edwin Saul <edwinsaulpm@gmail.com>
2023-12-07 17:59:39 -0500

    ejemplo DOM ok

--- DOM/01_ejemplo_simple/main.js
+++ DOM/01_ejemplo_simple/main.js
@@ -1,1 +1,24 @@
 console.log("hola mundo desde js")
+
+function generarColorHex() {
+  // Genera un color aleatorio para hexadecimal
+  const color = Math.floor(Math.random()*16777215).toString(16);
+  return '#' + '0'.repeat(6 - color.length) + color;
+};
+
+function cambiarColorFondo(fondo) {
+  document.body.style.backgroundColor=fondo;
+};
+
+function cambiarColor() {
+  var colorGenerado=generarColorHex(); 
+  cambiarColorFondo(colorGenerado);
+  var etiqueta=document.getElementById("colorLabel")
+  etiqueta.innerText=colorGenerado;
+};
+
+const boton=document.getElementById("btn");
+boton.addEventListener("click",cambiarColor);
+
+cambiarColor()
+
